a77f413961428133900a984f38da545769ec3bab
ground83 <wiseguy830@naver.com>
2022-03-13 21:23:23 +0900

    static2

--- cdn/conf.py
+++ cdn/conf.py
@@ -2,9 +2,9 @@ import os
 
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME = "knft-storage"
+AWS_STORAGE_BUCKET_NAME = "knftstorage"
 AWS_S3_ENDPOINT_URL = "https://sfo3.digitaloceanspaces.com"
 AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400", "ACL": "public-read"}
-AWS_LOCATION = "https://knft-storage.sfo3.digitaloceanspaces.com"
-DEFAULT_FILE_STORAGE = "knft-storage.cdn.backends.MediaRootS3BotoStorage"
-STATICFILES_STORAGE = "knft-storage.cdn.backends.StaticRootS3BotoStorage"
+AWS_LOCATION = "https://knftstorage.sfo3.digitaloceanspaces.com"
+DEFAULT_FILE_STORAGE = "knftstorage.cdn.backends.MediaRootS3BotoStorage"
+STATICFILES_STORAGE = "knftstorage.cdn.backends.StaticRootS3BotoStorage"
